9183e3ec907fe46ee7932617c1cc216707bf1e8c
Mark Miller <mrkmiller@ucdavis.edu>
2020-08-06 22:12:36 -0700

    completed

--- 2020-08/yacht.js
+++ 2020-08/yacht.js
@@ -9,8 +9,8 @@ const categoryMap = {
   'sixes': rollDigit,
   'full house': rollFullHouse,
   'four of a kind': rollFourKind,
-  'little straight': rollLittleStraight,
-  'big straight': rollBigStraight,
+  'little straight': rollStraight,
+  'big straight': rollStraight,
   'choice': rollChoice
 }
 
@@ -20,7 +20,6 @@ export const score = (dice, category) => {
   //Ex.  return rollYacht(dice)
   //Ex2. return rollDigit(dice)
 };
-
 
 function rollYacht(dice) {
   const diceSet = new Set(dice);
@@ -46,13 +45,38 @@ function rollDigit(dice, category) {
   return sum(winners)
 }
 
-function rollFourKind() {}
+function rollFourKind(dice) {
+  const groups = rollGroupings(dice)
+  const key = Object.keys(groups).find(key => groups[key] >= 4)
 
-function rollLittleStraight() {}
+  if (key) {
+    return key * 4
+  }
 
-function rollBigStraight() {}
+  return 0
+}
 
-function rollFullHouse() {}
+function rollStraight(dice, category) {
+  const straight = (category === 'little straight') ? '1,2,3,4,5' : '2,3,4,5,6'
+  const sorted = dice.sort().join();
+
+  if (sorted === straight) {
+    return 30
+  }
+
+  return 0
+}
+
+function rollFullHouse(dice) {
+  const groups = rollGroupings(dice)
+  const counts = Object.values(groups)
+
+  if (counts.includes(2) && counts.includes(3)) {
+    return sum(dice)
+  }
+
+  return 0;
+}
 
 function rollChoice(dice) {
   return sum(dice)
@@ -62,4 +86,15 @@ function sum(scores) {
   return scores.reduce((previous, current) => {
     return previous + current
   }, 0)
+}
+
+function rollGroupings(dice) {
+  const groups = {}
+
+  dice.forEach(current => {
+    let previous = groups[current] ? groups[current] : 0
+    groups[current] = ++previous
+  })
+
+  return groups
 }
